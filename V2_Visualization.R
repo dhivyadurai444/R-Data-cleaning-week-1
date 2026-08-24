0858964aca0db1a086c7fec8fc3ad4b25151d428
dhivyadurai444 <dhivyadurai444@gmail.com>
2026-08-24 20:49:31 +0530

    Week 2 Data Visualization

---
+++ V2_Visualization.R
@@ -0,0 +1,8 @@
+# Week 2 - Data Visualization using R
+
+library(ggplot2)
+
+titanic <- read.csv("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv")
+
+# View the first few rows
+head(titanic)
